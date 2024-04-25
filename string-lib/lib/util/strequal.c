77a0bf4f7c603bf23037603af56410acc6dce569
samirbuch <samir.buch@temple.edu>
2024-04-25 14:00:11 -0400

    string_lib: strequal is less robust because i CBA

--- string-lib/lib/util/strequal.c
+++ string-lib/lib/util/strequal.c
@@ -2,15 +2,18 @@
 // Created by Samir Buch on 4/18/24.
 //
 
-int strequal(const char* s1, const char* s2) {
-    int is_equal = 1;
+#include "../str_utils.h"
 
-    while(*(s1++) != '\0' && *(s2++) != '\0') {
-        if(*s1 != *s2) {
-            is_equal = 0;
-            break;
-        }
+int strequal(const char* s1, const char* s2) {
+    size_s s1Len = s_strlen(s1);
+    size_s s2Len = s_strlen(s2);
+
+    if(s1Len != s2Len) return 0;
+
+    for(int i = 0; i < s1Len; i++) {
+        if(s1[i] != s2[i])
+            return 0;
     }
 
-    return is_equal;
+    return 1;
 }
